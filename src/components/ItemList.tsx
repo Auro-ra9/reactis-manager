3e9904c7b9a3979078f025d352b444ec1bb9047d
Auro-ra9 <ashnaahammed9@gmail.com>
2025-02-13 23:54:54 +0530

    style: updated grid layout for better responsiveness

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -11,7 +11,7 @@ const ItemList = () => {
   //throw error message
   if (error) return <p className="text-center text-red-500">{error}</p>;
 
-  return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-4">
+  return <div className="grid grid-cols-1 md:grid-cols-2  gap-6 mt-4">
     {items.map((item) => (
         <ItemCard key={item.id} item={item} />
       ))}
