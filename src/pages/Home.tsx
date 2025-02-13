b9e87748caf965a0d0f74e5c7324c7c9910f3312
Auro-ra9 <ashnaahammed9@gmail.com>
2025-02-13 18:02:43 +0530

    Feat (Home): Build Home page and integrate ItemForm

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import ItemForm from "../components/ItemForm";
+
+const Home = () => {
+  return (
+    <div className="max-w-4xl mx-auto p-4">
+      <ItemForm/> 
+      {/* if gets time sortfilter */}
+      {/* item list */}
+    </div>
+  );
+};
+
+export default Home;
