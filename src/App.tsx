3944556c7711ed0c01c32c3ee886add45a3906fa
Auro-ra9 <ashnaahammed9@gmail.com>
2025-02-14 00:28:02 +0530

    feat: Integrate Navbar component with theme toggler in App - added Navbar to App for theme toggling functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,23 @@ import "./App.css";
 import Home from "./pages/Home";
 import { ItemProvider } from "./context/ItemContext";
 import { Toaster } from "react-hot-toast";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
   return (
-    <div className="min-h-screen bg-white transition-all">
+    <div
+      className={`min-h-screen ${
+        theme === "light" ? "bg-white" : "bg-gray-800"
+      } transition-all`}
+    >
       <ItemProvider>
-        <Toaster/>
+        <Toaster />
+        <Navbar toggleTheme={toggleTheme} theme={theme} />
         <Home />
       </ItemProvider>
     </div>
