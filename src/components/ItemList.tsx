f51d3c6a0a11eb72e7f7745aefb2c4030cabf44e
Auro-ra9 <ashnaahammed9@gmail.com>
2025-02-14 15:01:35 +0530

    refactor: add conditional rendering in ItemList for filtered and normal items
    - modified ItemList to dynamically render filteredItems if available.

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -3,19 +3,28 @@ import ItemCard from "./ItemCard";
 import Spinner from "./Spinner";
 
 const ItemList = () => {
-  const { items, isLoading, error } = useItems();
-  
+  const { items, isLoading, filteredItems, error } = useItems();
+
+  //list to display, filtered items if available, else items itself
+  const displayItems = filteredItems.length > 0 ? filteredItems : items;
   //replace with Loading Spinner Component Later
-  if (isLoading) return <p className="text-center"><Spinner/></p>;
+  if (isLoading)
+    return (
+      <p className="text-center">
+        <Spinner />
+      </p>
+    );
 
   //throw error message
   if (error) return <p className="text-center text-red-500">{error}</p>;
 
-  return <div className="grid grid-cols-1 md:grid-cols-2  gap-6 mt-4">
-    {items.map((item) => (
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2  gap-6 mt-4">
+      {displayItems.map((item) => (
         <ItemCard key={item.id} item={item} />
       ))}
-  </div>;
+    </div>
+  );
 };
 
 export default ItemList;
